18f31e20928405258e8a5bb99c4a5263ac39e95c
Erik Westrup <erik.westrup@icloud.com>
2025-01-15 09:30:36 +0000

    Wed, Jan 15, 2025, 3:00 PM +05:30

--- project.py
+++ project.py
@@ -1,15 +1,20 @@
-def read_visa_validity_days():
+from datetime import date
+
+def read_visa_validity_days() -> int:
     while True:
         try:
             return int(input("How many days is your entry stamp valid? "))
         except ValueError:
             pass
 
+def last_day_valid_from_today(days: int) -> date:
+    pass
+
 
 def main():
-    visa_days = read_visa_validity_days()
-    last_day = calc_last_day_from_today(visa_days)
-    print(visa_days)
+    visa_days: date = read_visa_validity_days()
+    last_day: date = last_day_valid_from_today(visa_days)
+    print(last_day)
 
 if __name__ == '__main__':
     main()
