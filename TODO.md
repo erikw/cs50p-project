5bbe20ba497adfde6435c356f5af8aa57d2280f7
Erik Westrup <erik.westrup@icloud.com>
2025-01-15 11:13:09 +0000

    Sketching out the features

--- TODO.md
+++ TODO.md
@@ -5,9 +5,6 @@
 
 
 # Ideas
-* Use argparse
-* Use the ascii art lib figlet to print a fancy title
-* Try using type hints and the mypy check.
 * Use Emoji in outputs ✈️leave country
 * Ask which country they visit, print link to visainfo page
 * Use lib for managing the menu with questions?
--- project.py
+++ project.py
@@ -1,7 +1,26 @@
+import argparse
+import sys
+
+from pyfiglet import Figlet
+
 from datetime import date, timedelta
 
 
-def read_visa_validity_days() -> int:
+TERM_WIDTH = 120
+
+
+def validity_days_args() -> int:
+    parser = argparse.ArgumentParser(
+        description="Helps you calculate the last valid day to stay in a country given an entry stamp in your passport"
+    )
+    parser.add_argument(
+        "-d", type=int, help="Number of days your entry is valid e.g. 90 days"
+    )
+    args = parser.parse_args()
+    return args.d
+
+
+def validity_days_input() -> int:
     while True:
         try:
             return int(input("How many days is your entry stamp valid? "))
@@ -10,15 +29,36 @@ def read_visa_validity_days() -> int:
 
 
 def last_day_valid_from_today(days: int) -> date:
-    delta = timedelta(days=days)
-    return date.today() + delta # + 1?????
+    if days < 1:
+        raise ValueError("An entry stamp is always at least one day valid.")
+
+    delta = timedelta(days=days - 1)  # -1 because the entry day counts as day 1.
+    return date.today() + delta
 
 
-def main():
-    visa_days: int = read_visa_validity_days()
+def welcome_screen() -> str:
+    figlet: Figlet = Figlet(width=TERM_WIDTH, justify="center")
+    # figlet.setFont(font='standard') # The only font guaranteed to exist it seems.
+    figlet.setFont(font="slant")
+    ascii = figlet.renderText("Visa Exit Calculator")  # TODO decide proper name.
+    return ascii
+
+
+def main() -> int:
+    print(welcome_screen())
+
+    visa_days: int
+    if len(sys.argv) > 1:
+        visa_days= validity_days_args()
+    else:
+        visa_days = validity_days_input()
+
     last_day: date = last_day_valid_from_today(visa_days)
-    print(last_day)
+    print("You need to leave the country latest on this day (before midnight):")
+    print(last_day.isoformat())
+    print(f"i.e. {last_day.strftime('%A %d, %B %Y')}")
 
+    return 0
 
 if __name__ == "__main__":
     main()
